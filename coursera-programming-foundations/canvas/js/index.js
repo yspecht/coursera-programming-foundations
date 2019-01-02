8da6cb696df7f758c9f96e9226ab6dba944fc124
yspecht <43762490+yspecht@users.noreply.github.com>
2019-01-02 16:27:19 -0500

    Add files via upload

---
+++ coursera-programming-foundations/canvas/js/index.js
@@ -0,0 +1,9 @@
+function doLime() {
+  var dd1 = document.getElementById("d1");
+  dd1.style.backgroundColor="lime";
+}
+
+function doYellow() {
+  var dd1 = document.getElementById("d1");
+  dd1.style.backgroundColor="yellow";
+}
